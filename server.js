dffd4acea8019b5fbb001885ec7ff6e372f9da77
PiyushChakerwarty <piyushchakerwarty97@gmail.com>
2021-12-24 12:00:13 +0530

    procfile added

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const connectDB=require('./server/database/connection')
 const app = express()
 
 dotenv.config({ path: 'config.env' })
-const PORT=process.env.PORT || 5001
+const PORT = process.env.PORT || 5001
 
 //log request
 app.use(morgan('tiny'));
